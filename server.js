b93f4d817d458261f249fdd5c197ab9133740afb
Gustavo Moreira Santos <141965903+GustavoMSantoss@users.noreply.github.com>
2024-06-12 17:18:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -19,11 +19,9 @@ const config = {
 
 app.use(cors());
 app.use(express.json());
-
-// Servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'front-game')));
 
-// Rota para atualizar informações de veículo e peça
+// Rota para atualizar as informações do veículo e peça
 app.post('/atualizarInfo', async (req, res) => {
     const { veiculo, peca, quilometragemAtual, quilometragemTroca } = req.body;
 
@@ -46,74 +44,34 @@ app.post('/atualizarInfo', async (req, res) => {
     }
 });
 
-// Rota para fornecer os dados do veículo e peça
+// Rota para obter todas as informações da tabela personagem
 app.get('/characters', async (req, res) => {
     try {
         await sql.connect(config);
         const request = new sql.Request();
-
+        
+        // Consulta para obter todos os dados da tabela personagem
         const result = await request.query("SELECT * FROM personagem");
-        const dados = result.recordset;
+        const personagens = result.recordset;
 
-        res.json(dados);
+        res.json(personagens);
     } catch (error) {
-        console.error('Erro ao buscar dados do veículo e peça:', error);
+        console.error('Erro ao buscar dados:', error);
         res.status(500).json({ error: 'Erro ao buscar dados.' });
     }
 });
 
-// Rota para inserir usuário
-app.post('/inserirUsuario', async (req, res) => {
-    const { usuario, senha } = req.body;
-
-    try {
-        await sql.connect(config);
-        const request = new sql.Request();
-        await request.query(`
-            MERGE INTO usuario AS target
-            USING (VALUES ('${usuario}', '${senha}')) AS source (usuario, senha)
-            ON target.usuario = source.usuario
-            WHEN MATCHED THEN
-                UPDATE SET senha = source.senha
-            WHEN NOT MATCHED THEN
-                INSERT (usuario, senha) VALUES (source.usuario, source.senha);
-        `);
-        res.status(200).send('Usuario cadastrado com sucesso.');
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Erro ao inserir usuario.');
-    }
-});
-
-// Rota para validar usuário
-app.get('/validarUsuario', async (req, res) => {
-    try {
-        const { usuario, senha } = req.query;
-        await sql.connect(config);
-        const request = new sql.Request();
-        const userResult = await request.query(`SELECT * FROM usuario WHERE usuario = '${usuario}' AND senha = '${senha}'`);
-        const user = userResult.recordset[0];
-        if (user === undefined) {
-           return res.status(404).json({ error: 'Usuario não cadastrado' });
-        }
-
-        return res.json({ usuario, senha });
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao buscar dados do usuario.' });
-    }
-});
-
-// Rotas para servir os arquivos HTML
+// Rota para servir o arquivo HTML principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'front-game/login.html'));
 });
 
-app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, 'front-game/dashboard.html'));
-});
-
 app.get('/game', (req, res) => {
     res.sendFile(path.join(__dirname, 'front-game/game.html'));
+});
+
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'front-game/dashboard.html'));
 });
 
 // Iniciar o servidor
